41e6e17ff593831f1a533a29a90260e3a20a4f3e
Phượng <22133044@student.hcmute.edu.vn>
2025-05-29 14:01:48 +0700

    QLNP

---
+++ BS Layer/BLNghiPhep.cs
@@ -0,0 +1,52 @@
+﻿using QuanLyNhanSu_3Tang_ADO.DB_Layer;
+using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace QuanLyNhanSu_3Tang_ADO.BS_Layer
+{
+    public class BLNghiPhep
+    {
+        DBMain db = new DBMain();
+
+        public DataSet LayNghiPhep()
+        {
+            string sql = @"SELECT np.MaNV, np.MaThang, np.NgayNghiPhep, np.GhiChu, nv.Ho + ' ' + nv.Ten AS TenNV, t.MoTa AS TenThang
+                           FROM NghiPhep np 
+                           JOIN NhanVien nv ON np.MaNV = nv.MaNV
+                           JOIN Thang t ON np.MaThang = t.MaThang";
+            return db.ExecuteQueryDataSet(sql, CommandType.Text);
+        }
+
+        public bool ThemNghiPhep(string maNV, string maThang, int ngayNghi, string ghiChu, ref string err)
+        {
+            string sql = "INSERT INTO NghiPhep (MaNV, MaThang, NgayNghiPhep, GhiChu) VALUES (@MaNV, @MaThang, @Ngay, @GhiChu)";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@MaNV", maNV);
+            cmd.Parameters.AddWithValue("@MaThang", maThang);
+            cmd.Parameters.AddWithValue("@Ngay", ngayNghi);
+            cmd.Parameters.AddWithValue("@GhiChu", ghiChu);
+            return db.MyExecuteNonQuery(cmd, CommandType.Text, ref err);
+        }
+
+        public bool CapNhatNghiPhep(string maNV, string maThang, int ngayNghi, string ghiChu, ref string err)
+        {
+            string sql = "UPDATE NghiPhep SET NgayNghiPhep = @Ngay, GhiChu = @GhiChu WHERE MaNV = @MaNV AND MaThang = @MaThang";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@Ngay", ngayNghi);
+            cmd.Parameters.AddWithValue("@GhiChu", ghiChu);
+            cmd.Parameters.AddWithValue("@MaNV", maNV);
+            cmd.Parameters.AddWithValue("@MaThang", maThang);
+            return db.MyExecuteNonQuery(cmd, CommandType.Text, ref err);
+        }
+
+        public bool XoaNghiPhep(string maNV, string maThang, ref string err)
+        {
+            string sql = "DELETE FROM NghiPhep WHERE MaNV = @MaNV AND MaThang = @MaThang";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@MaNV", maNV);
+            cmd.Parameters.AddWithValue("@MaThang", maThang);
+            return db.MyExecuteNonQuery(cmd, CommandType.Text, ref err);
+        }
+    }
+}
--- obj/Debug/net8.0-windows/QuanLyNhanSu_3Tang_ADO.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/QuanLyNhanSu_3Tang_ADO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyNhanSu_3Tang_ADO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b35444262f6dc73d2f1b4559b7d8719c28065fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+815bc43c876813306144bca59d784877e7fc28be")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyNhanSu_3Tang_ADO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyNhanSu_3Tang_ADO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
