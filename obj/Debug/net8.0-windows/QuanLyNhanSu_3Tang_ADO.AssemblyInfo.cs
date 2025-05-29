38389ee4554791aa14f53057f117db88693e30b8
Nguyễn Thị Hồng Thơ <22151305@student.hcmute.edu.vn>
2025-05-30 05:24:40 +0700

    fix conflict

--- obj/Debug/net8.0-windows/QuanLyNhanSu_3Tang_ADO.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/QuanLyNhanSu_3Tang_ADO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyNhanSu_3Tang_ADO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9521a34508f62f187cc9f79633b86726c52c689c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+041f4933a52e6533b6225b31838928e081b80efb")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyNhanSu_3Tang_ADO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyNhanSu_3Tang_ADO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
